90a593fd3f577786502c30ffcb66291aaf6dc9cd
lukehedger <luke.hedger@gmail.com>
2018-01-24 18:42:45 +0000

    Add style to server log

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
+const boxen = require('boxen')
+const chalk = require('chalk')
 
 const { RPC_ENDPOINT } = require('./constants')
 const Query = require('./query')
@@ -12,9 +14,18 @@ const resolvers = {
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 
-server.start(() =>
-  console.log(`
-  ⚡️  Server is running on localhost:4000
-  ⛓  RPC_ENDPOINT => ${RPC_ENDPOINT}
-`)
-)
+server.start(() => {
+  const message =
+    chalk.yellow.bold('eth-graphql') +
+    '\n\nServer is running on http://localhost:4000\n\n' +
+    `RPC_ENDPOINT => ${RPC_ENDPOINT}`
+
+  console.log(
+    boxen(message, {
+      align: 'center',
+      borderColor: 'magenta',
+      margin: 1,
+      padding: 1,
+    })
+  )
+})
