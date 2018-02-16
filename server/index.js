d7b9023fd66be5645546d47515a8c907e9ff4cfb
lukehedger <luke.hedger@gmail.com>
2018-02-16 18:11:39 +0000

    Update server to pass event emitter and RPC request factory in GraphQL context

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga')
 const boxen = require('boxen')
 const chalk = require('chalk')
+const EventEmitter = require('events')
 
 const { RPC_ENDPOINT } = require('./constants')
 const Query = require('./query')
 const schema = require('./schema.js')
+const { makeRPCRequest } = require('./util')
+
+const eventEmitter = new EventEmitter()
+
+eventEmitter.setMaxListeners(0)
 
 const typeDefs = schema
 
@@ -12,7 +18,11 @@ const resolvers = {
   Query: Query,
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers })
+const context = {
+  makeRPCRequest: makeRPCRequest(RPC_ENDPOINT, eventEmitter),
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers, context })
 
 server.start(() => {
   const message =
